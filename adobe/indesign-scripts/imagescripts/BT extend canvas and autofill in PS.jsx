b49b5cd9a9d407d488eac46e34da466aec6cda1f
Eli <subarachnoid@gmail.com>
2020-06-12 08:44:06 +1200

    fixed up a couple things from home,started  working on save to [Resources] folder

--- adobe/indesign-scripts/imagescripts/BT extend canvas and autofill in PS.jsx
+++ adobe/indesign-scripts/imagescripts/BT extend canvas and autofill in PS.jsx
@@ -5,41 +5,26 @@ var undoName = "trim selected frames to bleedbounds"
 
 function main(){
   var bleed = app.activeDocument.documentPreferences.documentBleedTopOffset;
-  var sel = app.activeDocument.selection;
+  var mySelection = app.activeDocument.selection[0];
 
-  for (var i=0; i < sel.length; i++){
-    var mySelection = app.activeDocument.selection[i];
+  var myFrame = {};
+  myFrame.topLeftY = mySelection.geometricBounds[0],
+  myFrame.topLeftX = mySelection.geometricBounds[1],
+  myFrame.bottomRightY = mySelection.geometricBounds[2],
+  myFrame.bottomRightX = mySelection.geometricBounds[3];
 
-    var myFrame = {};
-    myFrame.topLeftY = mySelection.geometricBounds[0],
-    myFrame.topLeftX = mySelection.geometricBounds[1],
-    myFrame.bottomRightY = mySelection.geometricBounds[2],
-    myFrame.bottomRightX = mySelection.geometricBounds[3];
+  var pageBounds = {};
+  pageBounds.topLeftY = mySelection.parentPage.bounds[0],
+  pageBounds.topLeftX = mySelection.parentPage.bounds[1],
+  pageBounds.bottomRightY = mySelection.parentPage.bounds[2],
+  pageBounds.bottomRightX = mySelection.parentPage.bounds[3];
 
-    var pageBounds = {};
-    pageBounds.topLeftY = mySelection.parentPage.bounds[0],
-    pageBounds.topLeftX = mySelection.parentPage.bounds[1],
-    pageBounds.bottomRightY = mySelection.parentPage.bounds[2],
-    pageBounds.bottomRightX = mySelection.parentPage.bounds[3];
+  var bleedBounds = {};
+  bleedBounds.topLeftY = pageBounds.topLeftY - bleed,
+  bleedBounds.topLeftX = pageBounds.topLeftX - bleed,
+  bleedBounds.bottomRightY = pageBounds.bottomRightY + bleed,
+  bleedBounds.bottomRightX = pageBounds.bottomRightX + bleed;
 
-    var bleedBounds = {};
-    bleedBounds.topLeftY = pageBounds.topLeftY - bleed,
-    bleedBounds.topLeftX = pageBounds.topLeftX - bleed,
-    bleedBounds.bottomRightY = pageBounds.bottomRightY + bleed,
-    bleedBounds.bottomRightX = pageBounds.bottomRightX + bleed;
-
-    if (myFrame.topLeftY < bleedBounds.topLeftY){
-      mySelection.geometricBounds = [bleedBounds.topLeftY, myFrame.topLeftX, myFrame.bottomRightY, myFrame.bottomRightX];
-      myFrame.topLeftY = bleedBounds.topLeftY;}
-    if (myFrame.topLeftX < bleedBounds.topLeftX){
-      mySelection.geometricBounds = [myFrame.topLeftY, bleedBounds.topLeftX, myFrame.bottomRightY, myFrame.bottomRightX];
-      myFrame.topLeftX = bleedBounds.topLeftX;}
-    if (myFrame.bottomRightY > bleedBounds.bottomRightY){
-      mySelection.geometricBounds = [myFrame.topLeftY, myFrame.topLeftX, bleedBounds.bottomRightY, myFrame.bottomRightX];
-      myFrame.bottomRightY = bleedBounds.bottomRightY;}
-    if (myFrame.bottomRightX > bleedBounds.bottomRightX){
-      mySelection.geometricBounds = [myFrame.topLeftY, myFrame.topLeftX, myFrame.bottomRightY, bleedBounds.bottomRightX];
-      myFrame.bottomRightX = bleedBounds.bottomRightX;}
 }}
 
 //DEBUG CODE
