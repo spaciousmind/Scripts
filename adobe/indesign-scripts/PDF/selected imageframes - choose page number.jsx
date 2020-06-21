6cea2dc314335d87688c8f9050f89c883799814a
Eli <subarachnoid@gmail.com>
2020-06-22 08:17:09 +1200

    got choose page number working!

---
+++ adobe/indesign-scripts/PDF/selected imageframes - choose page number.jsx
@@ -0,0 +1,66 @@
+$.writeln("==========================")
+var undoName = "Choose Page Number of link";
+app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.FAST_ENTIRE_SCRIPT, undoName);
+
+
+function main(){
+
+var mySelection = app.selection;
+var chosenPage = prompt("Choose Page Number:", "1");
+chosenPage = parseInt(chosenPage);
+$.writeln("chosenPage =" + chosenPage);
+
+
+
+for (var i=0; i < mySelection.length; i++){
+
+  if (mySelection[i].hasOwnProperty("graphics") && (mySelection[i].graphics.length != 0)) {
+    var myFrame = mySelection[i];
+    contentSwitch(myFrame, chosenPage);}
+
+  else if ((mySelection[i] instanceof PDF)||(mySelection[i] instanceof ImportedPage)) {
+    var myFrame = mySelection[i].parent;
+    contentSwitch(myFrame, chosenPage);}
+
+  else {
+    alert ("Please select a frame with placed PDF, AI or INDD file!");}
+}
+
+function contentSwitch(myFrame, chosenPage){
+  $.writeln("content switch chosenPage =" + chosenPage);
+  $.writeln(myFrame.allGraphics[0].constructor.name)
+  switch (myFrame.allGraphics[0].constructor.name) {
+  case "PDF":
+    currentFile = myFrame.pdfs[0];
+    placeFile (currentFile, chosenPage);
+    break;
+  case "ImportedPage":
+    currentFile = myFrame.importedPages[0];
+    placeFile (currentFile, chosenPage);
+    break;
+  default:
+    alert("No PDF, AI or INDD file in the selected frame!")}
+}
+
+function placeFile(currentFile, chosenPage){
+  $.writeln("placeFile chosenPage =" + chosenPage);
+
+
+  app.pdfPlacePreferences.pageNumber = chosenPage;
+  app.importedPageAttributes.pageNumber = chosenPage;
+  $.writeln("placeFile");
+  filePath = currentFile.itemLink.filePath;
+  $.writeln("filePath =" + filePath);
+  $.writeln("myFrame =" + myFrame);
+  $.writeln("currentFile =" + currentFile);
+  try{
+    filetoPlace = File(filePath);
+    $.writeln("filetoPlace =" + filetoPlace)
+    currentFile.place(filetoPlace, false);
+  } catch (e){
+    if(currentFile.itemLink.status == LinkStatus.LINK_MISSING || currentFile.itemLink.status == LinkStatus.LINK_INACCESSIBLE){
+      alert("Missing link:\r" + filePath + "\r\rNo changes have been made!");
+    }
+  }
+}
+}
--- adobe/indesign-scripts/imagescripts/resize image to chosen PPI.jsx
+++ adobe/indesign-scripts/imagescripts/resize image to chosen PPI.jsx
@@ -1,1 +1,64 @@
-﻿/** * @@@BUILDINFO@@@ EffectivePPI.js This script rescale selected images according to the chosen resolution */if (parseFloat(app.version) < 6) {    main();} else {    app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.FAST_ENTIRE_SCRIPT, "Expand State Abbreviations");}function main() {    var multiplicador = prompt("Choose effective PPI:", "300");    if (isNumber(multiplicador) && multiplicador > 0) {        var effectivePPIThreshold = multiplicador;        var myDocument = app.activeDocument;        var userHoriz = myDocument.viewPreferences.horizontalMeasurementUnits;        var userVert = myDocument.viewPreferences.verticalMeasurementUnits;        myDocument.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.points;        myDocument.viewPreferences.verticalMeasurementUnits = MeasurementUnits.points;        for (var i = 0; i < app.selection.length; i++) {            var theImageFrame = app.selection[i];            var theImage = theImageFrame.images[0]            var effectivePPI = String(theImage.effectivePpi);            var imageBounds = theImage.geometricBounds;            var imageWidth = imageBounds[3] - imageBounds[1];            var imageHeight = imageBounds[2] - imageBounds[0];            effectivePPI.match(/(\d+),(\d+)/);            var ppiH = effectivePPI.replace(/(\d+),(\d+)/, '$1');            var ppiV = effectivePPI.replace(/(\d+),(\d+)/, '$2');            if (ppiH == ppiV) {                var theRatio = ppiH / effectivePPIThreshold;                var newImageWidth = imageWidth * theRatio;                var newImageHeight = imageHeight * theRatio;                theImage.geometricBounds = [imageBounds[0], imageBounds[1], imageBounds[0] + newImageHeight, imageBounds[1] + newImageWidth];                theImageFrame.fit(FitOptions.frameToContent);            } else {                alert("Horizontal and vertical resolutions are not equal.");            }        }        myDocument.viewPreferences.horizontalMeasurementUnits = userHoriz;        myDocument.viewPreferences.verticalMeasurementUnits = userVert;    } else {        alert("Invalid input.");    }}function isNumber(n) {    return !isNaN(parseFloat(n)) && isFinite(n);}
+﻿/**
+* @@@BUILDINFO@@@ EffectivePPI.js
+This script rescale selected images according to the chosen resolution
+*/
+
+
+if (parseFloat(app.version) < 6) {
+main();
+} else {
+app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.FAST_ENTIRE_SCRIPT, "Expand State Abbreviations");
+}
+
+
+
+function main() {
+var multiplicador = prompt("Choose effective PPI:", "300");
+
+if (isNumber(multiplicador) && multiplicador > 0) {
+var effectivePPIThreshold = multiplicador;
+
+var myDocument = app.activeDocument;
+var userHoriz = myDocument.viewPreferences.horizontalMeasurementUnits;
+var userVert = myDocument.viewPreferences.verticalMeasurementUnits;
+myDocument.viewPreferences.horizontalMeasurementUnits = MeasurementUnits.points;
+myDocument.viewPreferences.verticalMeasurementUnits = MeasurementUnits.points;
+
+
+for (var i = 0; i < app.selection.length; i++) {
+
+var theImageFrame = app.selection[i];
+var theImage = theImageFrame.images[0]
+var effectivePPI = String(theImage.effectivePpi);
+var imageBounds = theImage.geometricBounds;
+var imageWidth = imageBounds[3] - imageBounds[1];
+var imageHeight = imageBounds[2] - imageBounds[0];
+
+effectivePPI.match(/(\d+),(\d+)/);
+
+var ppiH = effectivePPI.replace(/(\d+),(\d+)/, '$1');
+var ppiV = effectivePPI.replace(/(\d+),(\d+)/, '$2');
+
+if (ppiH == ppiV) {
+var theRatio = ppiH / effectivePPIThreshold;
+var newImageWidth = imageWidth * theRatio;
+var newImageHeight = imageHeight * theRatio;
+theImage.geometricBounds = [imageBounds[0], imageBounds[1], imageBounds[0] + newImageHeight, imageBounds[1] + newImageWidth];
+theImageFrame.fit(FitOptions.frameToContent);
+} else {
+alert("Horizontal and vertical resolutions are not equal.");
+}
+
+}
+
+myDocument.viewPreferences.horizontalMeasurementUnits = userHoriz;
+myDocument.viewPreferences.verticalMeasurementUnits = userVert;
+
+} else {
+alert("Invalid input.");
+}
+}
+
+function isNumber(n) {
+return !isNaN(parseFloat(n)) && isFinite(n);
+}
